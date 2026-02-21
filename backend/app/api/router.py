7d9ab1a8f4b6783c960b469d2521dcc6252edeb8
cristiandcunivers-can <cristiandcunivers@gmail.com>
2026-02-21 23:32:49 +0000

    router.py

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,1 +1,10 @@
+from fastapi import APIRouter
+from app.api.routes import health
 
+api_router = APIRouter()
+
+api_router.include_router(
+    health.router,
+    prefix="/health",
+    tags=["Health"]
+)
